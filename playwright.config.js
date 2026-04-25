0dc28baad68552af79ffba325aff51e13925ae3e
Om Korde <om.korde@GTS-GLOBAL.COM>
2026-04-25 13:17:03 +0530

    Fix junit reporter

--- playwright.config.js
+++ playwright.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? [['html'], ['JUnit', { outputFile: 'result.xml' }], ['allure-playwright']] : [['html'], ['allure-playwright']],
+  reporter: process.env.CI ? [['html'], ['junit', { outputFile: 'result.xml' }], ['allure-playwright']] : [['html'], ['allure-playwright']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
