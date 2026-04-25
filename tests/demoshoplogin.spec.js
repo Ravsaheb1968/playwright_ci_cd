30c994464124c43ef896bafe67854e24417b0884
Om Korde <om.korde@GTS-GLOBAL.COM>
2026-04-25 14:39:06 +0530

    add data file local to framework

--- tests/demoshoplogin.spec.js
+++ tests/demoshoplogin.spec.js
@@ -4,7 +4,7 @@ import {test} from "../testfixtures/fixtures.js";
 
 import {getExcelData} from "../utilities/exceldata.js";
 
-const testData = getExcelData("D:/PlaywrightData.xlsx","Data");
+const testData = getExcelData("../testdata/PlaywrightData.xlsx","Data");
 
 test("Demoshop Login Page", async({loginPage})=>{
 
