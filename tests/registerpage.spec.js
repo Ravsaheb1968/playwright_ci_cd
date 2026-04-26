2e327c3cabf5d735f9e457fd7894ffa73999f614
Om Korde <omkorde.rs2002@gmail.com>
2026-04-26 22:39:41 +0530

    Add Parllelism for Register

--- tests/registerpage.spec.js
+++ tests/registerpage.spec.js
@@ -1,5 +1,5 @@
 import { RegisterPage } from "../pages/registerpage.js";
-import {test} from "../testfixtures/fixtures.js";
+import { test } from "../testfixtures/fixtures.js";
 
 import { getExcelData } from "../utilities/exceldata.js";
 
@@ -9,8 +9,25 @@ const filePath = path.resolve("testdata/Playwright_Data.xlsx");
 
 const testData = getExcelData(filePath, "Users");
 
-test("Register the User", async({registerPage})=>{
-    await registerPage.navigate();
-    console.log("The name of the user is " + testData[0].FirstName);
-    await registerPage.RegisterUser(testData[0].Gender,testData[0].FirstName, testData[0].LastName, testData[0].Email,testData[0].Password,testData[0].ConfirmPassword);
+
+test.describe.parallel("Register User", async () => {
+
+    testData.forEach(user => {
+        let i = 0;
+        test(`Register the User ${user.Email}`, async ({ page, registerPage }) => {
+
+            let i = +1;
+            await registerPage.navigate();
+            console.log("The name of the user is " + testData[0].FirstName);
+            await registerPage.RegisterUser(testData[0].Gender, testData[0].FirstName, testData[0].LastName, testData[0].Email, testData[0].Password, testData[0].ConfirmPassword);
+            if (i == 10) {
+                page.close();
+                break;
+
+            }
+        })
+    })
+
 })
+
+
