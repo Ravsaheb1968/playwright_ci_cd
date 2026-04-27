7edefeff6cede93b30037a7f4a3c8c5788eda63d
Om Korde <omkorde.rs2002@gmail.com>
2026-04-27 21:31:36 +0530

    Update the Gender Select in Register Page

--- pages/registerpage.js
+++ pages/registerpage.js
@@ -1,11 +1,12 @@
 import { Basepage } from "./Basepage";
 
-export class RegisterPage extends Basepage{
+export class RegisterPage extends Basepage {
 
-    constructor(page){
+    constructor(page) {
         super(page);
-        this.Registerlink = this.page.getByRole("link",{name:"Register"});
-        this.Gender = this.page.locator("#gender-male");
+        this.Registerlink = this.page.getByRole("link", { name: "Register" });
+        this.GenderMale = this.page.locator("#gender-male");
+        this.GenderFemale = this.page.locator("#gender-female");
         this.Firstname = this.page.locator("#FirstName");
         this.Lastname = this.page.locator("#LastName");
         this.Email = this.page.locator("#Email");
@@ -13,20 +14,28 @@ export class RegisterPage extends Basepage{
         this.ConfirmPass = this.page.locator("#ConfirmPassword");
 
         // click on the Register button
-        this.Registerbtn = this.page.getByRole("textbox", {name:"register-button"});
+        this.Registerbtn = this.page.getByRole("textbox", { name: "register-button" });
 
     }
 
-    async RegisterUser(Gender,FirstName,LastName,Email,Password,ConfirmPassword){
+    async RegisterUser(Gender, FirstName, LastName, Email, Password, ConfirmPassword) {
 
         await this.click(this.Registerlink);
-        await this.click(this.Gender);
+
+        if (Gender === "Male") {
+            await this.click(this.GenderMale);
+        }
+        else {
+            await this.click(this.GenderFemale);
+        }
+
         await this.fill(this.Firstname, FirstName);
-        await this.fill(this.Lastname,LastName);
+        await this.fill(this.Lastname, LastName);
         await this.fill(this.Email, Email);
         await this.fill(this.Password, Password);
-        await this.fill(this.ConfirmPass,ConfirmPassword);
-        
+        await this.fill(this.ConfirmPass, ConfirmPassword);
+        await this.click(this.Registerbtn);
+
         // await this.Registerlink.click();
         // await this.Gender.click();
         // await this.Firstname.fill(FirstName);
@@ -34,6 +43,6 @@ export class RegisterPage extends Basepage{
         // await this.Email.fill(Email);
         // await this.Password.fill(Password);
         // await this.ConfirmPass.fill(ConfirmPassword);
-        
+
     }
 }
