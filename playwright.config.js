ddecfda53bdc463b2e5acce1579e333361e40d3f
Om Korde <omkorde.rs2002@gmail.com>
2026-04-28 15:06:53 +0530

    Add the changes in end to end flow

--- playwright.config.js
+++ playwright.config.js
@@ -19,12 +19,12 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 1,
+  retries: process.env.CI ? 2 : 0,
 
   timeout: 30 * 1000,
   expect: { timeout: 5 * 1000 },
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 4 : undefined,
+  workers: process.env.CI ? 4 : 6,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI ? [['html'], ['junit', { outputFile: 'result.xml' }], ['allure-playwright']] : [['html'], ['allure-playwright']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
