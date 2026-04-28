c37d8c202c79497e1ffc0003104652f5a13041d4
Om Korde <omkorde.rs2002@gmail.com>
2026-04-28 22:54:43 +0530

    Add Register to add and remove prod

--- tests/endtoend.spec.js
+++ tests/endtoend.spec.js
@@ -13,8 +13,8 @@ test.describe.parallel("End to flow Login to Remove Cart ", ()=>{
     testData.forEach(user =>{
         test(`Login to Add to Cart ${user.Email} ` , async({page,loginPage,registerPage,logoutPage,addressPage,addCart })=>{
             await registerPage.navigate();
-            // await registerPage.RegisterUser(user.Gender,user.FirstName, user.LastName, user.Email, user.Password, user.ConfirmPassword);
-            // await logoutPage.LogoutApplication();
+            await registerPage.RegisterUser(user.Gender,user.FirstName, user.LastName, user.Email, user.Password, user.ConfirmPassword);
+            await logoutPage.LogoutApplication();
             await loginPage.login(user.Email, user.Password);
             await addressPage.UserAddress(user.FirstName, user.LastName, user.Email, user.Company, user.Country,user.City, user.Address1, user.Address2,user.ZipPostalCode,user.PhoneNumber,user.FaxNumber);
             await addCart.AddToCart();
